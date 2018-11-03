c896b8c2ea2025f2c57c55229bc4a9dbec8829b5
zcolin <57521534@qq.com>
2018-11-03 17:00:52 +0800

    # 可自定义Recyclerview布局，传入的布局可以没有下拉刷新及空布局

--- libZRecyclerView/src/main/java/com/zcolin/gui/zrecyclerview/ZRecycleViewDivider.java
+++ libZRecyclerView/src/main/java/com/zcolin/gui/zrecyclerview/ZRecycleViewDivider.java
@@ -99,8 +99,7 @@ public class ZRecycleViewDivider extends RecyclerView.ItemDecoration {
             final View child = parent.getChildAt(i);
             //所有header和footer都不画线
             if (child.getTag() != null && child.getTag() instanceof String) {
-                String tag = ((String) child.getTag(R.id.srv_reserved_ivew));
-                if ("reservedView".equals(tag)) {
+                if ("reservedView".equals(child.getTag(R.id.srv_reserved_ivew)) || "reservedView".equals(child.getTag())) {
                     continue;
                 }
             }
